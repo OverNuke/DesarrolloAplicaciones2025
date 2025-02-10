d06bca494296f0bb8980b6c193faad4d18a7cac9
zS22017021 <ksfgarcia24@gmail.com>
2025-02-10 17:30:18 -0600

    Primera Actividad 10/02/2025

---
+++ .idea/PrimeraPractica/StringCharacters.java
@@ -0,0 +1,33 @@
+import java.util.regex.Pattern;
+
+public class StringCharacters {
+    public static void main(String[] args) {
+        String text = "Este autobús va muy lento, si no acelera llegaré muy tarde a clases" +
+                "no podré presentar el examen, el profesor no va a creerme, reprobaré" +
+                " y ¿Qué será de mí? Ni siquiera es mi culpa, pero todos actuarán como" +
+                " or to taje arms against a sea of troubles," +
+                " así fuera.";
+        text = text.toLowerCase();
+
+        int spaces = 0
+        , vowels = 0
+        , letters = 0;
+
+        // --------------------------------------------------------------------------
+        String [] TextSplitList;
+
+        spaces = text.split("\\s").length;
+
+        text = text.replaceAll("\\s","");
+
+        Pattern regexExpVowel = Pattern.compile("[à-ü |aeiou]", Pattern.CASE_INSENSITIVE);
+        vowels = text.split(regexExpVowel.pattern()).length;
+
+        Pattern regexExpLetters = Pattern.compile("[a-z|à-ü]", Pattern.CASE_INSENSITIVE);
+         letters = text.split(regexExpLetters.pattern()).length;
+
+        // --------------------------------------------------------------------------
+        System.out.println("The text contained vowels: " +vowels+ "\n" + "Consonants:" +
+                (letters - vowels) + "\nspaces: "+ spaces);
+    }
+}
