c949e936691c35a6b61c66f608cf01a64769c98a
zS22017021 <ksfgarcia24@gmail.com>
2025-02-10 18:09:21 -0600

    Primera Actividad 10/02/2025

--- .idea/PrimeraPractica/StringCharacters.java
+++ .idea/PrimeraPractica/StringCharacters.java
@@ -5,7 +5,7 @@ public class StringCharacters {
         String text = "Este autobús va muy lento, si no acelera llegaré muy tarde a clases y" +
                 "no podré presentar el examen, el profesor no va a creerme, reprobaré" +
                 " y ¿Qué será de mí? Ni siquiera es mi culpa, pero todos actuarán como" +
-                " or to taje arms against a sea of troubles," +
+                " or to take arms against a sea of troubles," +
                 " así fuera.";
         text = text.toLowerCase();
 
@@ -15,18 +15,20 @@ public class StringCharacters {
 
         // --------------------------------------------------------------------------
         String [] TextSplitList;
+
         // Existe una división adicional en el arreglo 10/02/2025
         spaces = text.split("\\s").length - 1;
 
         text = text.replaceAll("\\s","");
 
-        Pattern regexExpVowel = Pattern.compile("[à-ü |aeiou]", Pattern.CASE_INSENSITIVE);
-        vowels = text.split(regexExpVowel.pattern()).length;
+        Pattern regexExpVowel = Pattern.compile("[^à-ü |^aeiou]", Pattern.CASE_INSENSITIVE);
+        vowels = text.replaceAll(regexExpVowel.pattern(), "").length();
 
-        Pattern regexExpLetters = Pattern.compile("[a-z|à-ü]", Pattern.CASE_INSENSITIVE);
-        letters = text.split(regexExpLetters.pattern()).length;
+        Pattern regexExpLetters = Pattern.compile("[^a-z|^à-ü]", Pattern.CASE_INSENSITIVE);
+        letters = text.replaceAll(regexExpLetters.pattern(),"").length();
 
         // --------------------------------------------------------------------------
+        System.out.println("S22017021 - 10/02/2025");
         System.out.println("The text contained vowels: " +vowels+ "\n" + "Consonants:" +
                 (letters - vowels) + "\nspaces: "+ spaces);
     }
